8b56582174e0932b5d252de90232f8d708ebbb30
putuyoga <putuyoga@outlook.com>
2015-04-27 11:42:12 +0700

    tambah appbar cari dan share

---
+++ PuteriIndonesia/DetailPage.xaml.cs
@@ -0,0 +1,102 @@
+﻿using PuteriIndonesia.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Phone.UI.Input;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace PuteriIndonesia
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DetailPage : Page
+    {
+        Puteri _selected;
+
+        public DetailPage()
+        {
+            this.InitializeComponent();
+            HardwareButtons.BackPressed += HardwareButtons_BackPressed;
+        }
+
+        /// <summary>
+        /// Handle Back Button Key
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void HardwareButtons_BackPressed(object sender, BackPressedEventArgs e)
+        {
+            Frame frame = Window.Current.Content as Frame;
+            if (frame == null)
+            {
+                return;
+            }
+
+            if (frame.CanGoBack)
+            {
+                frame.GoBack();
+                e.Handled = true;
+            }
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            int id = Convert.ToInt32(e.Parameter);
+
+            // Dapatkan Puteri dengan id xx
+            _selected = App.ViewModel.ListPuteri.FirstOrDefault(p => p.Id == id);
+            this.DataContext = _selected;
+
+            DataTransferManager dtManager = DataTransferManager.GetForCurrentView();
+            dtManager.DataRequested += dtManager_DataRequested;
+        }
+
+        /// <summary>
+        /// Event Handler Method untuk pencarian puteri 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void Cari_Click(object sender, RoutedEventArgs e)
+        {
+            Uri link = new Uri("http://www.bing.com/search?q=" + _selected.Nama, UriKind.Absolute);
+            await Launcher.LaunchUriAsync(link);
+        }
+
+        /// <summary>
+        /// Event Handler Method untuk share puteri
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Share_Click(object sender, RoutedEventArgs e)
+        {
+            Windows.ApplicationModel.DataTransfer.DataTransferManager.ShowShareUI();
+        }
+
+        private void dtManager_DataRequested(DataTransferManager sender, DataRequestedEventArgs args)
+        {
+            args.Request.Data.Properties.Title = _selected.Nama;
+            args.Request.Data.Properties.Description = _selected.Asal;
+            args.Request.Data.SetWebLink(new Uri("http://putuyoga.com"));
+        }
+    }
+}
