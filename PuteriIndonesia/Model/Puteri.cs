835157be9fed2822dd0fae83c76744015fc23092
putuyoga <putuyoga@outlook.com>
2015-04-27 11:14:37 +0700

    commit awal

---
+++ PuteriIndonesia/Model/Puteri.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuteriIndonesia.Model
+{
+    public class Puteri
+    {
+        /// <summary>
+        /// Identifier untuk gambar
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Berisi nama lengkap
+        /// </summary>
+        public string Nama { get; set; }
+
+        /// <summary>
+        /// Tahun kapan menjadi puteri Indonesia
+        /// </summary>
+        public int Tahun { get; set; }
+
+        /// <summary>
+        /// Asal daerah provinsi puteri Indonesia
+        /// </summary>
+        public string Asal { get; set; }
+
+        /// <summary>
+        /// Biografi singkat sang puteri Indonesia
+        /// </summary>
+        public string Biografi { get; set; }
+
+        /// <summary>
+        /// Lokasi foto
+        /// </summary>
+        public string Foto
+        {
+            get { return "/Assets/Foto/" + Id + ".jpg"; }
+        }
+    }
+}
---
+++ PuteriIndonesia/ViewModels/MainViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using PuteriIndonesia.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace PuteriIndonesia.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// untuk menampung items Puteri Indonesia
+        /// </summary>
+        private List<Puteri> _listPuteri;
+
+        /// <summary>
+        /// Memberikan akses items Puteri Indonesia
+        /// </summary>
+        public List<Puteri> ListPuteri
+        {
+            get { return _listPuteri; }
+            set
+            {
+                if(_listPuteri != value)
+                {
+                    _listPuteri = value;
+                    NotifyPropertyChanged("ListPuteri");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Mengambil items Puteri Indonesia dari local dari deserialize
+        /// </summary>
+        public async void GetData()
+        {
+            Uri dataUri = new Uri("ms-appx:///Assets/DataPuteri.json");
+
+            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(dataUri);
+            string jsonText = await FileIO.ReadTextAsync(file);
+            ListPuteri = JsonConvert.DeserializeObject<List<Puteri>>(jsonText);
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
